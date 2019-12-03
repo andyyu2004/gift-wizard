dbf207e6aa90e5922d0362d734a01e3b1c9b39ce
wang-ximing <wxm19990222love@sina.com>
2019-12-03 13:23:21 -0500

    added styling and markup for areaofinterest

--- client/src/components/users/AreaOfInterest.tsx
+++ client/src/components/users/AreaOfInterest.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {MouseEvent, useState} from 'react';
 import { getRandom } from '../../util/array';
 import './AreaOfInterest.css';
+import deleteimg from '../../images/cancel_icon.png';
 
 type PropType = {
   interests: string[],
@@ -9,15 +10,32 @@ type PropType = {
 const cssclasses = ["label music", "label travels", "label web", "label yoga", "label dog"];
 
 const AreaOfInterest: React.FC<PropType> = ({ interests }) => {
+  const [editable, setEditable]=useState(false);
   return (
-    <div id="interests">
-      {interests && interests.length !== 0 && <h5>Personal Area of Interests</h5>}
-      {interests && interests.map((interest, i) => <span key={i} className={getRandom(cssclasses)}>{interest}</span>)}
-      {/* <span className="label music">Music</span>
-      <span className="label travel">Travel</span>
-      <span className="label web">Web Programming</span>
-      <span className="label yoga">Yoga</span>
-      <span className="label dog">Dogs</span> */}
+    <div>
+      <div id="interests">
+        {interests && interests.length !== 0 && <h5>Personal Area of Interests</h5>}
+        {interests && interests.map((interest, i) => 
+        <span key={i} className={getRandom(cssclasses)}>
+          {interest} 
+          {editable && <img onClick={()=>{}} className="deleteInterest" src={deleteimg}/>}
+        </span>)}
+        {/* <span className="label music">Music</span>
+        <span className="label travel">Travel</span>
+        <span className="label web">Web Programming</span>
+        <span className="label yoga">Yoga</span>
+        <span className="label dog">Dogs</span> */}
+      </div>
+      {editable && 
+        <div className="addInterest"> 
+          <input type="text" onChange={e => {/*input a interest*/}}/>
+          <button className="addInterestButton" onClick={() =>{/* add an interest*/}}>Add Interest</button>
+        </div>
+      }
+      <div className="interestButtons">
+        <button className="editInterestButton" onClick={() => setEditable(true)}>Edit</button>
+        <button className="saveInterestButton" onClick={() => setEditable(false)}>Save</button>
+      </div>
     </div>
   );
 };
